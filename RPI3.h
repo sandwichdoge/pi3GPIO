747480589dc696cab3a2ea1ce334ba1ce55f53ba
sandwichdoge <38368906+sandwichdoge@users.noreply.github.com>
2018-07-04 09:19:33 -0400

    added SET_ALT(), fixed descriptions

--- RPI3.h
+++ RPI3.h
@@ -5,13 +5,15 @@
 #include <errno.h>
 
 
-//BCM2837 Broadcom processor GPIO peripheral access
+//BCM2837 Broadcom ARM processor GPIO peripheral access library
 //For Pi2 v1.2 to Pi3+ (https://www.raspberrypi.org/documentation/hardware/raspberrypi/)
 //sandwichdoge@gmail.com
 
 
-//See datasheet page 6 for this part
-#define BCM_PERI_BASE 0x3F000000  
+/*See datasheet page 6 for this part.
+It is explicitly stated that all access must be done through physical address (0x3F000000)
+0x200000 is the offset of GPIO peripheral from peripheral base address in memory*/
+#define BCM_PERI_BASE 0x3F000000
 #define GPIO_BASE (BCM_PERI_BASE + 0x200000)  
 #define BLOCK_SZ (4*1024)
 
@@ -27,14 +29,13 @@ peri_t GPIO;
 extern peri_t GPIO;
 
 
-#define SET_IN(p) *(GPIO.addr + ((p)/10)) &= ~(7<<(((p)%10)*3))
+#define SET_IN(p) *(GPIO.addr + ((p)/10)) &= ~(7<<((p%10)*3))
 /*
 Function    : set pin 'p' to INPUT mode
 p           : GPIO pin number
 GPIO.addr   : mapped (virtual) base address of GPIO in /dev/mem
 
-RPI3 GPIO has 40 pins,
-each pin's Function Select represented by 3 bits (datasheet page 90-92)
+Each GPIO pin's Function Select represented by 3 bits (datasheet page 90-92)
 we need to flip said bits to 000 to change its function to INPUT mode
 
 GPIO.addr + p/10        : address of register segment that contains pin 'p' in mapped memory
@@ -54,7 +55,7 @@ p/10 &= ~(7<<((p%10)*3)): set the 3 bits that represent pin p%10 in segment p/10
 */
 
 
-#define SET_OUT(p) *(GPIO.addr + ((p)/10)) |= (1<<(((p)%10)*3))
+#define SET_OUT(p) *(GPIO.addr + ((p)/10)) |= (1<<((p%10)*3))
 /*
 Function    : set pin number 'p' to OUTPUT mode
 Important   : SET_IN must be called first to set all bits to 0
@@ -65,46 +66,62 @@ GPIO.addr + p/10        : see SET_IN()
 (p%10)*3                : the position of 3 bits that represent pin p%10 in segment p/10
 1<<((p%10)*3)           : set first bit of the 3 FSEL bits to 1, thus changing its function to OUTPUT
                         : SET_OUT() needs to be called first to ensure the other 2 bits are 00
-p/10 |= 1<<(((p)%10)*3) : set the 3 bits that represent pin p%10 in segment p/10 to 001
+p/10 |= 1<<((p%10)*3) : set the 3 bits that represent pin p%10 in segment p/10 to 001
+
+*/
+
+
+#define SET_ALT(p, i) *(GPIO.addr + (p/10)) |= ((i<=3?(i+4):(i==4?3:2))<<((p%10)*3))
+/*
+Function    : set pin number 'p' to ALT mode, for alt functions see datasheet page 102
+p           : GPIO pin number
+i           : ALT function number
+GPIO.addr   : mapped (virtual) base address of GPIO in /dev/mem
+
+GPIO.addr + p/10        : see SET_IN()
+For explanation of the function see datasheet page 92-94
 
 */
 
 
 #define GPIO_SET *(GPIO.addr + 7)
 /*
-GPIOSET is 7 bytes away from GPIO base address
+GPIOSET is 7*4 bytes away from GPIO base address
 it has 2 segments, each 4bytes long, they can be considered a single 8bytes segment
 each bit represents 1 pin, flipping it to 1 is equivalent to writing 1 to said pin
-e.g. GPIO_SET = 1<<p;
-this will write 1 to GPIO pin number 'p'
 last 10bits are reserved
 WRITING "0" HAS NO EFFECT, USE GPIO_CLR INSTEAD!
+
+e.g. GPIO_SET = 1<<p;
+this will write 1 to GPIO pin number 'p'
 */
 
 #define GPIO_CLR *(GPIO.addr + 10)
 /*
-GPIOCLR is 10 bytes away from GPIO base address
+GPIOCLR is 10*4 bytes away from GPIO base address
 it has 2 segments, each 4bytes long, they can be considered a single 8bytes segment
 each bit represents 1 pin, flipping it to 1 is equivalent to writing 0 to said pin
 if the pin is in INPUT mode, this has no effect
-e.g. GPIO_CLR = 1<<p;
-this will write 0 to GPIO pin number 'p'
 last 10bits are reserved
 writing "0" has no effect.
+
+e.g. GPIO_CLR = 1<<p;
+this will write 0 to GPIO pin number 'p'
 */
 
 
 #define GPIO_READ(p) (*(GPIO.addr + 13) & (1<<(p)))
 /*
-GPIOLEV is 13 bytes away from GPIO base address
+GPIOLEV is 13*4 bytes away from GPIO base address
 it has 2 segments, each 4bytes long, they can be considered a single 8bytes segment
 return 1 if high, 0 if low (0V)
+
 e.g. val = GPIO_READ(p);
 this will return status of pin number 'p'
 */
 
 
-/*initialize GPIO, always call this before proceeding with any GPIO modifications*/
+/*Initialize GPIO, always call this before proceeding with any GPIO modifications*/
 int map_gpio(peri_t *pGPIO)
 {
     pGPIO->devmem_fd = open("/dev/mem", O_RDWR|O_SYNC);
